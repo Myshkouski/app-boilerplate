3e16c112c7ad3420387202562569fd63da2889dd
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2017-12-09 02:09:14 +0300

    redirect from "/" to "/tickers"

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,42 +1,34 @@
 <template lang="pug">
-v-app(light)
-  v-navigation-drawer.hidden-lg-and-up.orange.lighten-1(
-    fixed
-    v-model="ui.drawer"
-    app
-  )
-    v-list(dense)
-      v-list-tile(@click="")
-        v-list-tile-action
-          v-icon(dark) mdi-home
-        v-list-tile-content
-          v-list-tile-title
-            span Home
-      v-list-tile(@click="")
-        v-list-tile-action
-          v-icon mdi-contact-mail
-        v-list-tile-content
-          v-list-tile-title
-            span Contact
-  v-toolbar.orange.lighten-1(fixed app)
-    v-toolbar-side-icon(@click.stop="ui.drawer = !ui.drawer")
-      v-icon mdi-menu
-    //- v-avatar.gray.lighten-1
-      v-icon mdi-person
-    v-toolbar-title Application
-  v-content
-    nuxt
-  v-bottom-nav(absolute :value="true" :active.sync="ui.bottomNav.active" color="transparent")
-    v-btn(flat color="gray" value="recent")
-      span Recent
-      v-icon mdi-history
-    v-btn(flat color="gray" value="favorites")
-      span Favorites
-      v-icon mdi-heart
-    v-btn(flat color="gray" value="nearby")
-      span Nearby
-      v-icon mdi-map-marker
-  //- v-footer.black(app)
+v-app
+	v-toolbar.hidden-lg-and-up.green.lighten-2(fixed app)
+		//- v-toolbar-side-icon.hidden-md-and-down(@click.stop="ui.drawer = !ui.drawer")
+			v-icon mdi-menu
+		v-toolbar-title Application
+	v-navigation-drawer#drawer.hidden-md-and-down.green.lighten-2(
+		fixed
+		touchless
+		app
+	)
+		v-list(dense)
+			v-toolbar.green.lighten-2()
+				v-toolbar-title Application
+				v-spacer
+				v-btn(flat fab to="/account")
+					v-avatar
+						v-icon person
+			v-list-tile(v-for="nav in ui.nav" :key="nav.index" :to="nav.href")
+				v-list-tile-action
+					v-icon {{ nav.icon }}
+				v-list-tile-content
+					v-list-tile-title
+						span {{ nav.name }}
+	v-content
+		nuxt
+	v-bottom-nav.hidden-lg-and-up.white(fixed app :value="true" )
+		v-btn(v-for="nav in ui.nav" :key="nav.index" :to="nav.href" flat color="gray")
+			span {{ nav.name }}
+			v-icon {{ nav.icon }}
+	//- v-footer.black(app)
 </template>
 
 <script>
@@ -44,21 +36,34 @@ export default {
   data() {
     return {
       ui: {
-        drawer: null,
-        bottomNav: {
-          active: null
-        }
+        drawer: false,
+        nav: [ {
+            name: 'Tickers',
+            icon: 'trending_up',
+            href: '/tickers'
+          },
+          {
+            name: 'Account',
+            icon: 'person',
+            href: '/account'
+          }
+        ].map( ( item, index ) => Object.assign( item, {
+          index
+        } ) )
       }
     }
   }
 }
 </script>
 
+<style>
+@import url("https://unpkg.com/vuetify@0.17.3/dist/vuetify.min.css");
+@import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons");
+</style>
 <style lang="styl" src="vuetify/src/stylus/main.styl"></style>
 <style lang="styl" src="vuetify/src/stylus/bootstrap.styl"></style>
-<style lang="scss" src="mdi/scss/materialdesignicons.scss"></style>
-
+<!-- <style lang="scss" src="mdi/scss/materialdesignicons.scss"></style> -->
 <style lang="sass" scoped>
-.app-layout
-  overflow: auto
+#drawer
+	min-height: 100vh
 </style>
