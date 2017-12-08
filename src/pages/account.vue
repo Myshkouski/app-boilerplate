fc68f981fb19f31b70e361f85bc435c3133047d3
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2017-12-09 02:07:04 +0300

    credentials inputs

---
+++ src/pages/account.vue
@@ -0,0 +1,56 @@
+<template lang="pug">
+v-container
+  v-card
+    span {{ user }}
+    v-text-field(
+      name="input-id"
+      label="id"
+      id="input-id"
+      :value="$store.state.credentials.id"
+      @change="onIdChange"
+    )
+    v-text-field(
+      name="input-secret"
+      label="secret"
+      id="input-secret"
+      :value="$store.state.credentials.secret"
+      @change="onSecretChange"
+    )
+    v-btn(@click.stop="loadAccountInfo") Save credentials
+    v-avatar
+      v-icon person
+</template>
+
+<script>
+import * as exmo from '~/api/exmo'
+
+export default {
+  data() {
+    return {
+      user: null
+    }
+  },
+
+  methods: {
+    onIdChange( id ) {
+      this.$store.commit( 'id', id )
+    },
+    onSecretChange( secret ) {
+      this.$store.commit( 'secret', secret )
+    },
+    loadAccountInfo() {
+      const {
+        id: key,
+        secret
+      } = this.$store.state.credentials
+
+      return exmo.user( {
+        key,
+        secret
+      } ).then( res => {
+        this.user = res
+      } ).catch( console.error )
+    }
+  }
+}
+</script>
