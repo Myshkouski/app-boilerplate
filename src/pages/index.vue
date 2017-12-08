b70713dc5324e6660026870011e178104790c4aa
Alexei Myshkouski <alexeimyshkouski@gmail.com>
2017-12-09 02:12:08 +0300

    redirect to tickers

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,56 +1,12 @@
-<template lang="pug">
-v-container(fluid fill-width)
-	v-tabs(grow v-model="ui.tabs.active")
-		v-tabs-bar.orange.lighten-1
-			v-tabs-item(
-				v-for="tab in data.tabs"
-				:key="tab.key"
-				:href="`#${tab.key}`"
-				ripple
-			)
-				span {{ tab.key }}
-		v-tabs-slider
-		v-tabs-items
-			v-tabs-content(
-				v-for="tab in data.tabs"
-				:key="tab.key"
-				:id="tab.key"
-			)
-				v-card(flat)
-					v-card-text {{ tab.text }}
-</template>
+<template></template>
 
 <script>
 export default {
-  data() {
-    return {
-      ui: {
-        tabs: {
-          active: null
-        }
-      },
-      intl: {},
-      dev: {},
-      data: {
-        tabs: [{
-          key: 'tab-1',
-          text: 'Tab 1'
-        }, {
-          key: 'tab-2',
-          text: 'Tab 2'
-        }]
-      }
-    }
-  },
-
-  methods: {
-    next () {
-      this.active = this.tabs[(this.tabs.indexOf(this.active) + 1) % this.tabs.length]
-    }
+  fetch( {
+    params,
+    redirect
+  } ) {
+    redirect( 301, '/tickers' )
   }
 }
 </script>
-
-<style lang="sass">
-
-</style>
