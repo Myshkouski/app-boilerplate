e5a6107a961889d229afd0aff40ebe3e81f444e3
Мышковский Алексей Владимирович <myshkovskii@nbrb.by>
2018-01-12 16:26:21 +0300

    swiper, button

---
+++ src/components/swiper.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+div.root(
+  ref="wrapper"
+  )
+  div.wrapper
+    slot
+</template>
+
+<script>
+// import Touchable from '~/components/touchable' 
+export default {
+
+}
+</script>
+
+<style lang="sass" scoped>
+  .root, .wrapper
+    position: relative
+
+  .wrapper
+    top: 0
+    bottom: 0
+    left: 0
+    right: 0
+    transform-style: preserve-3d
+</style>
---
+++ src/pages/swiper.vue
@@ -0,0 +1,41 @@
+<template lang="pug">
+  div.box
+    div.wrapper(
+      ref="wrapper"
+      )
+      div.tracker(
+        v-for="tracker in trackers"
+        :style="{ transform: `translateX(${ tracker.x }px) translateY(${ tracker.y }px)` }"
+        )
+</template>
+
+<script>
+import SwiperComponent from '~/components/swiper'
+export default {}
+</script>
+
+<style lang="sass" scoped>
+  .box
+    display: flex
+
+  .wrapper
+    position: relative
+    width: 300px
+    height: 300px
+    margin: auto
+    background-color: gray
+    transform-style: preserve-3d
+
+  .tracker
+    position: absolute
+    top: 0
+    bottom: 0
+    left: 0
+    right: 0
+    margin-top: -5px
+    margin-left: -5px
+    width: 10px
+    height: 10px
+    border-radius: 50%
+    background-color: red
+</style>
